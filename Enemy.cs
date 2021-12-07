ef86d8f7d333074b15af39ec7c4040797280aca7
JanaGames <95649063+JanaGames@users.noreply.github.com>
2021-12-07 22:43:27 +0300

    Movement simple
    Movement simple for mini games. all points placed on map

---
+++ Enemy.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public List<Transform> walkPoints;
+    
+    [HideInInspector]
+    public List<Transform> innerWalkPoints;
+    Rigidbody rigi;
+    public int currentPoint;
+    public int currentInnerPoint;
+    public bool readyWalk;
+    public bool readyAttack;
+    bool catched;
+    bool readyDestroy;
+    public int newPoint;
+    public int newInnerPoint;
+    public float speed;
+    public float timeAttack;
+    private IEnumerator coroutine;
+    Animator animator;
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!readyDestroy) 
+        {
+            if (!readyAttack) 
+               {
+                  if (currentPoint != newPoint) Movement(newPoint);
+                  if (currentPoint == newPoint && currentInnerPoint != newInnerPoint) MovementInner(newInnerPoint); 
+                  //no move;
+                  if (currentPoint == newPoint && currentInnerPoint == newInnerPoint) Actions();
+               }
+            if (GetComponent<FieldOfView>().CheckTargets()) Attack();
+            if (!GetComponent<FieldOfView>().CheckTargets()) readyAttack = false;
+        }
+        if (readyDestroy) 
+        {
+            if(!catched) 
+            {
+                GetComponent<Projectile>().Jump();
+                animator.SetTrigger("Jump");
+            }
+        }
+    }
+    void Actions() 
+    {
+        //start any animation or readyWalk;
+        //set anim[i].length to coroutine;
+        if (Random.Range(0, 2) == 1 && !readyWalk) 
+        {
+            readyWalk = false;
+            string currentAnimation = "SitFoot";
+            animator.Play(currentAnimation);
+            coroutine = SetParentIndex(Random.Range(3, 6));
+            StartCoroutine(coroutine);
+        }
+        else if (!readyWalk)
+        {
+            coroutine = SetParentIndex(0.2f);
+            StartCoroutine(coroutine);
+        }
+    }
+    void Attack() 
+    {
+        readyAttack = true;
+        coroutine = AttackPause(timeAttack);
+        StartCoroutine(coroutine);
+    }
+    public void MoveTo(int indexPoint) 
+    {
+        newPoint = indexPoint;
+        newInnerPoint = 0;
+    }
+    public void MoveToInner(int indexPoint, List<Transform> inners) 
+    {
+        if (indexPoint != 0) 
+        {
+            newInnerPoint = indexPoint;
+            innerWalkPoints = inners;
+        }
+        else 
+        {
+            //returnParentPoint = true;
+            newInnerPoint = 0;
+        }
+    }
+    void Movement(int indexPoint) 
+    {
+        readyWalk = false;
+        animator.SetTrigger("Move");
+        if (currentPoint < indexPoint) 
+        {
+            Vector3 direction = (walkPoints[currentPoint + 1].position - transform.position).normalized;
+            FaceTarget(walkPoints[currentPoint + 1].position);
+            rigi.MovePosition(transform.position + direction * speed * Time.deltaTime);
+        }
+        if (currentPoint > indexPoint) 
+        {
+            Vector3 direction = (walkPoints[currentPoint - 1].position - transform.position).normalized;
+            FaceTarget(walkPoints[currentPoint - 1].position);
+            rigi.MovePosition(transform.position + direction * speed * Time.deltaTime);
+        }
+        if (currentPoint == indexPoint) newPoint = currentPoint;
+    }
+    void MovementInner(int indexPoint) 
+    {
+        readyWalk = false;
+        animator.SetTrigger("Move");
+        if (currentInnerPoint < indexPoint) 
+        {
+            Vector3 direction = (innerWalkPoints[currentInnerPoint].position - transform.position).normalized;
+            FaceTarget(innerWalkPoints[currentInnerPoint].position);
+            rigi.MovePosition(transform.position + direction * speed * Time.deltaTime);
+        }
+        if (currentInnerPoint > indexPoint) 
+        {
+            if (indexPoint == 0) 
+            {
+                Vector3 direction = (walkPoints[currentPoint].position - transform.position).normalized;
+            FaceTarget(walkPoints[currentPoint].position);
+            rigi.MovePosition(transform.position + direction * speed * Time.deltaTime);
+            }
+            else 
+            {
+                Vector3 direction = (innerWalkPoints[currentInnerPoint-1].position - transform.position).normalized;
+            FaceTarget(innerWalkPoints[currentInnerPoint-1].position);
+            rigi.MovePosition(transform.position + direction * speed * Time.deltaTime);
+            }
+        }
+        if (currentInnerPoint == indexPoint) newInnerPoint = currentInnerPoint;
+    }
+    private IEnumerator SetParentIndex(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        readyWalk = true;
+    }
+    private IEnumerator AttackPause(float waitTime)
+    {
+        if (GetComponent<FieldOfView>().CheckTargets()) 
+        {
+            Vector3 target = GetComponent<FieldOfView>().GetNearestTargets(transform.position).position;
+            FaceTarget(target);
+            animator.SetTrigger("AttackDown");
+        }
+        yield return new WaitForSeconds(waitTime);
+        if (GetComponent<FieldOfView>().CheckTargets()) 
+        {
+            if (GetComponent<FieldOfView>().GetNearestTargets(transform.position).GetComponent<PlayerController>()) 
+            {
+                GetComponent<FieldOfView>().GetNearestTargets(transform.position).GetComponent<PlayerController>().stopped = true;
+                GetComponent<FieldOfView>().GetNearestTargets(transform.position).GetComponent<PlayerController>().Dead();
+            }
+        }
+        yield return new WaitForSeconds(2.0f);
+        if (GetComponent<FieldOfView>().CheckTargets()) 
+        {
+            Vector3 target = GetComponent<FieldOfView>().GetNearestTargets(transform.position).position;
+            GetComponent<Projectile>().SetData(target, transform.position);
+            readyDestroy = true;
+        }
+        if (!GetComponent<FieldOfView>().CheckTargets()) animator.SetTrigger("AttackReset");
+    }
+    void FaceTarget(Vector3 point)
+    {
+        Vector3 direction = (point - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))  catched = true;
+    }
+}
---
+++ LevelManager.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameManager gameManager;
+    public List<Transform> walkPoints;
+    public List<GameObject> enemiesPrefub;
+    List<GameObject> enemies = new List<GameObject>();
+    public int LevelPoint;
+    public int CurrentPoint;
+    public float timeAttack;
+    bool pointSet;
+
+    [HideInInspector]
+    static public int indexGen;
+
+    [HideInInspector]
+    static public int indexLevel;
+    private IEnumerator coroutine;
+    void Start()
+    {
+        LoadEnemiesByGen();
+        GetWalkPoints();
+        SetIndex();
+        SetPointsToEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemies.Count > 0) Set();
+        if (CurrentPoint > 0) CheckPoint();
+    }
+    void CheckPoint() 
+    {
+        if (CurrentPoint == LevelPoint) 
+        {
+            indexGen++;
+            gameManager.ReStartLevelGenUp(indexGen);
+        }
+    }
+    void Set() 
+    {
+        foreach(GameObject enemy in enemies) 
+        {
+            if (enemy.GetComponent<Enemy>().readyWalk && !pointSet) 
+            {
+                //if walk point have inner points
+                if (enemy.GetComponent<Enemy>().currentInnerPoint == 0 && walkPoints[enemy.GetComponent<Enemy>().currentPoint].GetComponent<WalkPoint>().innerPoints.Count != 0) 
+                {
+                    //next point or inner point;
+                    int value = Random.Range(0, 9);
+                    if (value >= 7) enemy.GetComponent<Enemy>().MoveTo(Random.Range(0, walkPoints.Count));
+                    else SetPoint(enemy, walkPoints[enemy.GetComponent<Enemy>().currentPoint].GetComponent<WalkPoint>().innerPoints);
+                }
+                //if walk point hav'nt inner points
+                if (enemy.GetComponent<Enemy>().currentInnerPoint == 0 && walkPoints[enemy.GetComponent<Enemy>().currentPoint].GetComponent<WalkPoint>().innerPoints.Count == 0) 
+                {
+                    //next point;
+                    enemy.GetComponent<Enemy>().MoveTo(Random.Range(0, walkPoints.Count));
+                }
+                //if enemy walk on inner points
+                if (enemy.GetComponent<Enemy>().currentInnerPoint != 0) 
+                {
+                    //next inner point or parent point;
+                    int value = Random.Range(0, 9);
+                    if (value >= 5) SetPoint(enemy, walkPoints[enemy.GetComponent<Enemy>().currentPoint].GetComponent<WalkPoint>().innerPoints);
+                    else enemy.GetComponent<Enemy>().MoveToInner(0, walkPoints[enemy.GetComponent<Enemy>().currentPoint].GetComponent<WalkPoint>().innerPoints);
+                }
+                coroutine = SetPoint(3.0f);
+                StartCoroutine(coroutine);
+            }
+            //if (walkPoints[enemy.GetComponent<Enemy>().currentPoint].GetComponent<WalkPoint>().innerPoints != 0)
+        }
+    }
+    void SetPoint(GameObject enemy, List<Transform> innerPoints) 
+    {
+        enemy.GetComponent<Enemy>().MoveToInner(Random.Range(1, innerPoints.Count + 1), innerPoints);
+    }
+    private IEnumerator SetPoint(float waitTime)
+    {
+        pointSet = true;
+        yield return new WaitForSeconds(waitTime);
+        pointSet = false;
+    }
+    void LoadEnemiesByGen() 
+    {
+        for(int i = 0; i < indexGen + 1; i++) 
+        {
+            enemies.Add(enemiesPrefub[i]);
+        }
+    }
+    void GetWalkPoints() 
+    {
+        for(int i = 0; i < transform.childCount; i++) 
+        {
+            if (transform.GetChild(i).CompareTag("WalkPoint")) 
+            {
+                walkPoints.Add(transform.GetChild(i));
+            }
+        }
+    }
+    void SetIndex() 
+    {
+        for(int i = 0; i < walkPoints.Count; i++) 
+        {
+            walkPoints[i].GetComponent<WalkPoint>().index = i;
+        }
+    }
+    void SetPointsToEnemies() 
+    {
+        for(int i = 0; i < enemies.Count; i++) 
+        {
+            enemies[i].GetComponent<Enemy>().walkPoints = walkPoints;
+            GameObject obj = Instantiate(enemies[i], walkPoints[i].position, enemies[i].transform.rotation);
+            enemies[i] = obj;
+            //
+            enemies[i].GetComponent<Enemy>().timeAttack = timeAttack;
+            //enemies[i].transform.position = walkPoints[0].position;
+        }
+    }
+}
